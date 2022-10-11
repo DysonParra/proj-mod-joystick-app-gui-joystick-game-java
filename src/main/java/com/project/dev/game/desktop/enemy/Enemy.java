927ab0f67eb85fa073e359d187c1ef8cd0f65e2e
Dyson <dysontilano@gmail.com>
2022-10-11 11:50:38 -0500

    Updated license of '*.java' files

--- src/main/java/com/project/dev/game/desktop/enemy/Enemy.java
+++ src/main/java/com/project/dev/game/desktop/enemy/Enemy.java
@@ -1,13 +1,13 @@
 /*
- * @fileoverview {FileName} se encarga de realizar tareas especificas.
+ * @fileoverview {Enemy} se encarga de realizar tareas especificas.
  *
  * @version             1.0
  *
  * @author              Dyson Arley Parra Tilano <dysontilano@gmail.com>
  * Copyright (C) Dyson Parra
  *
- * @History v1.0 --- La implementacion de {FileName} fue realizada el 31/07/2022.
- * @Dev - La primera version de {FileName} fue escrita por Dyson A. Parra T.
+ * @History v1.0 --- La implementacion de {Enemy} fue realizada el 31/07/2022.
+ * @Dev - La primera version de {Enemy} fue escrita por Dyson A. Parra T.
  */
 package com.project.dev.game.desktop.enemy;
 
